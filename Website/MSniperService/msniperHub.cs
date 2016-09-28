45f14947cca85d80e183fad96e79e3f3674388ed
MSX752 <msx752@users.noreply.github.com>
2016-09-28 23:39:42 +0300

    cleanup

--- Website/MSniperService/msniperHub.cs
+++ Website/MSniperService/msniperHub.cs
@@ -1,29 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.Caching;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
-using Microsoft.AspNet.SignalR;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNet.SignalR;
 using MSniperService.Cache;
 using MSniperService.Enums;
 using MSniperService.Models;
-using MSniperService.Statics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MSniperService
 {
     public class msniperHub : Hub
     {
-        private readonly static Timer pokemonTimer;
-        private readonly static Timer pokemonTop5Timer;
-        //public static DateTime pokemonLastCheck = DateTime.Now;
-        public static DateTime RateLastCheck = DateTime.Now;
-        private static readonly List<string> feeders = new List<string>();
-        private static readonly List<string> listeners = new List<string>();
-
         public static RareList defaultRareList = new RareList()
         {
             PokemonNames = new List<string>(){
@@ -32,32 +20,21 @@ namespace MSniperService
                                                 "blastoise", "magikarp", "dratini", "growlithe","pidgey"
                                              }
         };
-        
+
+        private static readonly List<string> feeders = new List<string>();
+        private static readonly List<string> listeners = new List<string>();
+        private static readonly Timer pokemonTimer;
+        private static readonly Timer pokemonTop5Timer;
+
         static msniperHub()
         {
-            pokemonTimer = new Timer(pokemonTick,null,
+            pokemonTimer = new Timer(pokemonTick, null,
                 (int)new TimeSpan(0, 0, 15).TotalMilliseconds,
                 (int)new TimeSpan(0, 0, 15).TotalMilliseconds);
 
             pokemonTop5Timer = new Timer(pokemonTop5Tick, null,
                 (int)new TimeSpan(0, 1, 0).TotalMilliseconds,
                 (int)new TimeSpan(0, 1, 0).TotalMilliseconds);
-        }
-
-        private static void pokemonTick(object state)
-        {
-            Instance.Clients.Group(HubType.Feeder.ToString()).sendPokemon();
-            Instance.Clients.Group(HubType.Listener.ToString()).ServerInfo(feeders.Count, listeners.Count); // server information feeder/hunter
-        }
-        
-        private static void pokemonTop5Tick(object state)
-        {
-            var pco = CacheManager<PokemonCounter>.Instance.GetCache("PokemonCounter");
-            if (pco == null)
-                pco = new PokemonCounter();
-            
-            var pokeInfos = pco.PCounter.OrderByDescending(p => p.Count).Take(5).ToList();
-            Instance.Clients.Group(HubType.Listener.ToString()).rate(pokeInfos);
         }
 
         public static IHubContext Instance
@@ -67,67 +44,6 @@ namespace MSniperService
                 return GlobalHost.ConnectionManager.GetHubContext<msniperHub>();
             }
         }
-
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            Leave();
-            return base.OnDisconnected(stopCalled);
-        }
-
-        #region signalr receivers
-
-        public void RecvPokemons(List<EncounterInfo> data)
-        {
-            if (data.Count > 0)
-            {
-                Clients.Group(HubType.Listener.ToString()).NewPokemons(data);
-            }
-        }
-
-        public string RecvIdentity()
-        {
-            try
-            {
-                //visitor joined
-                Join(HubType.Listener);
-                Clients.Client(this.Context.ConnectionId).ServerInfo(feeders.Count, listeners.Count);
-
-                var rlist = CacheManager<RareList>.Instance.GetCache("RareList");
-                if (rlist == null)
-                {
-                    rlist = defaultRareList;
-                    CacheManager<RareList>.Instance.AddCache(defaultRareList);
-                }
-
-                Clients.Client(this.Context.ConnectionId)
-                    .RareList(rlist.PokemonNames);
-                return "connection established";
-            }
-            catch (Exception e)
-            {
-                return e.Message;
-            }
-        }
-
-        public void Rate(string pokemonName)
-        {
-            //check snipped pokemons
-            var pco = CacheManager<PokemonCounter>.Instance.GetCache("PokemonCounter");
-            if (pco == null)
-                pco = new PokemonCounter();
-
-            pco.Increase(pokemonName);
-            CacheManager<PokemonCounter>.Instance.AddCache(pco);
-        }
-
-        public void Identity()
-        {
-            //feder joined
-            Join(HubType.Feeder);
-            Clients.Client(this.Context.ConnectionId).sendIdentity(this.Context.ConnectionId);
-        }
-
-        #endregion
 
         public void Join(HubType groupName)
         {
@@ -163,5 +79,82 @@ namespace MSniperService
                 }
             }
         }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Leave();
+            return base.OnDisconnected(stopCalled);
+        }
+
+        private static void pokemonTick(object state)
+        {
+            Instance.Clients.Group(HubType.Feeder.ToString()).sendPokemon();
+            Instance.Clients.Group(HubType.Listener.ToString()).ServerInfo(feeders.Count, listeners.Count); // server information feeder/hunter
+        }
+
+        private static void pokemonTop5Tick(object state)
+        {
+            var pco = CacheManager<PokemonCounter>.Instance.GetCache("PokemonCounter");
+            if (pco == null)
+                pco = new PokemonCounter();
+
+            var pokeInfos = pco.PCounter.OrderByDescending(p => p.Count).Take(5).ToList();
+            Instance.Clients.Group(HubType.Listener.ToString()).rate(pokeInfos);
+        }
+
+        #region signalr receivers
+
+        public void Identity()
+        {
+            //feder joined
+            Join(HubType.Feeder);
+            Clients.Client(this.Context.ConnectionId).sendIdentity(this.Context.ConnectionId);
+        }
+
+        public void Rate(string pokemonName)
+        {
+            //check snipped pokemons
+            var pco = CacheManager<PokemonCounter>.Instance.GetCache("PokemonCounter");
+            if (pco == null)
+                pco = new PokemonCounter();
+
+            pco.Increase(pokemonName);
+            CacheManager<PokemonCounter>.Instance.AddCache(pco);
+        }
+
+        public string RecvIdentity()
+        {
+            try
+            {
+                //visitor joined
+                Join(HubType.Listener);
+                Clients.Client(this.Context.ConnectionId).ServerInfo(feeders.Count, listeners.Count);
+
+                var rlist = CacheManager<RareList>.Instance.GetCache("RareList");
+                if (rlist == null)
+                {
+                    rlist = defaultRareList;
+                    CacheManager<RareList>.Instance.AddCache(defaultRareList);
+                }
+
+                Clients.Client(this.Context.ConnectionId)
+                    .RareList(rlist.PokemonNames);
+                return "connection established";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public void RecvPokemons(List<EncounterInfo> data)
+        {
+            if (data.Count > 0)
+            {
+                Clients.Group(HubType.Listener.ToString()).NewPokemons(data);
+            }
+        }
+
+        #endregion signalr receivers
     }
 }
