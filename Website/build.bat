7964184ce4bbcaeeb73f41c74e1418ac88dde142
MSX752 <msx752@users.noreply.github.com>
2016-11-13 20:37:43 +0300

    no message

--- Website/build.bat
+++ Website/build.bat
@@ -1,4 +1,4 @@
 echo off
 
 nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "MSniperService.sln" /property:Configuration=Release /property:Platform="Any CPU"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "MSniperService.sln" /p:Configuration=Release /p:Platform="Any CPU"
